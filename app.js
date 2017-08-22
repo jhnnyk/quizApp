12038d84fe4a59f2b2cc28fa20db3105c905dced
John Knotts <jhnnyk@gmail.com>
2017-08-22 16:55:51 -0600

    stub out functions

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+function startOver() {
+  // reset the score and start over
+  console.log("starting over...")
+}
+
+function showFinalScreen() {
+  console.log("showing the final screen...")
+}
+
+function showNextQuestion() {
+  console.log("getting the next question...")
+}
+
+function updateScore() {
+  console.log("updating the score...")
+}
+
+function checkAnswer() {
+  console.log("checking the answer...")
+}
+
+function startQuiz() {
+  console.log("starting the quiz...")
+}
+
 function renderWelcomePage() {
   console.log("rendering welcome page...")
 }
