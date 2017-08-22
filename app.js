61816800d66d9a926c0ad83c6d3874576607537a
John Knotts <jhnnyk@gmail.com>
2017-08-22 17:31:23 -0600

    show the welcome screen

--- app.js
+++ app.js
@@ -23,8 +23,24 @@ function startQuiz() {
   console.log("starting the quiz...")
 }
 
+function setWelcomeNavButtons() {
+  $('button').prop('disabled', true)
+  $('button.js-start-quiz').prop('disabled', false)
+}
+
 function renderWelcomePage() {
   console.log("rendering welcome page...")
+  const welcomeHTML = `
+    <h2>Welcome to the Mt. Evans Quiz!</h2>
+    <p>
+      This quiz is designed to educate and inform you of some fun facts about Mt. Evans.
+    </p>
+    <p>
+      Please start the quiz with the buttons below.
+    </p>`
+  
+  setWelcomeNavButtons()
+  $('.js-content').html(welcomeHTML)
 }
 
 function runQuiz() {
